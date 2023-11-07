8a421b50e13d49bc8f5930e284e92c3857b511bf
MrkMzp <markmazepaa@gmail.com>
2023-11-08 00:47:42 +0200

    Version 1

---
+++ main.c
@@ -0,0 +1,135 @@
+#define F_CPU 16000000UL
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+volatile unsigned long currentMillis = 0;
+volatile uint8_t dig[3] = {0 , 0 , 0};
+
+void DrawDigit(uint8_t digit)
+{
+	if(digit == 0)
+	{
+		PORTD &= 0xD0;
+		PORTC &=  0x34;
+	}
+	if(digit == 1)
+	{
+		PORTD &= 0x80;
+		PORTC &=  0x04;
+	}
+	if(digit == 2)
+	{
+		PORTD &= 0xB0;
+		PORTC &=  0x30;
+	}
+	if(digit == 3)
+	{
+		PORTD &= 0xB0;
+		PORTC &=  0x14;
+	}
+	if(digit == 4)
+	{
+		PORTD &= 0xC0;
+		PORTC &=  0x14;
+	}
+	if(digit == 5)
+	{
+		PORTD &= 0x70;
+		PORTC &=  0x14;
+	}
+	if(digit == 6)
+	{
+		PORTD &= 0x70;
+		PORTC &=  0x34;
+	}
+	if(digit == 7)
+	{
+		PORTD &= 0xA0;
+		PORTC &=  0x04;
+	}
+	if(digit == 8)
+	{
+		PORTD &= 0xF0;
+		PORTC &=  0x34;
+	}
+	if(digit == 9)
+	{
+		PORTD &= 0xF0;
+		PORTC &=  0x14;
+	}
+	return;
+}
+
+void ClearDisplay()
+{
+	PORTB = 0x00;
+	PORTC = 0x00;
+	PORTD = 0x00;
+	return;
+}
+
+void ShowSeconds()
+{
+	//first digit (x 0 0)
+	PORTB = 0x04;
+	DrawDigit(dig[2]);
+	ClearDisplay();
+	
+	//second (0 x 0)
+	PORTD = 0x02;
+	DrawDigit(dig[1]);
+	ClearDisplay();
+	
+	//third (0 0 x)
+	PORTB = 0x02;
+	DrawDigit(dig[0]);
+	ClearDisplay();
+}
+
+void timer1_init() 
+{
+	TCCR1B |= (1 << WGM12);
+	OCR1A = 125;  // For 1 ms (16 MHz CPU)
+	TIMSK1 |= (1 << OCIE1A);
+	TCNT1 = 0;
+}
+
+ISR(TIMER1_COMPA_vect) 
+{ 
+	currentMillis++;
+}
+
+
+
+int main()
+{
+	timer1_init();
+    sei();
+    
+    uint8_t seconds = 0;
+	
+	DDRB = 0x03;
+	PORTB = 0x00;
+	DDRD = 0xF4;
+	PORTD = 0x00;
+	DDRC = 0x3C;
+	PORTC = 0x00;
+
+    while (1) 
+    {
+		if(currentMillis == 999000)currentMillis = 0;
+		
+		seconds = currentMillis/1000;
+		
+		dig[0] = seconds % 10;
+		dig[1] = (seconds / 10) % 10;
+		dig[2] = (seconds / 100) % 10; 
+		
+		showSeconds();
+    }
+    	
+	return 0;
+}
---
+++ makefile
@@ -0,0 +1,24 @@
+	# Makefile for programming AVR in Geany 
+	FILENAME   = main
+	DEVICE     = atmega328p
+	PROGRAMMER = arduino
+	BAUD       = 115200
+	COMPILE    = "F:\AVRToolchain\avr8-gnu-toolchain-win32_x86_64\bin\avr-gcc.exe" -Wall -Os -mmcu=$(DEVICE)
+	REMOVE = C:\MinGW\msys\1.0\bin\rm.exe
+	PORT = 
+
+default: compile clean  # flash
+
+compile:
+	$(COMPILE) -c $(FILENAME).c -o $(FILENAME).o
+	$(COMPILE) -o $(FILENAME).elf $(FILENAME).o
+	"F:\AVRToolchain\avr8-gnu-toolchain-win32_x86_64\bin\avr-objcopy.exe" -j .text -j .data -O ihex $(FILENAME).elf $(FILENAME).hex 
+	"F:\AVRToolchain\avr8-gnu-toolchain-win32_x86_64\bin\avr-size.exe" --format=avr --mcu=$(DEVICE) $(FILENAME).elf
+
+flash:
+	#TODO add the path to AVR Dude # avrdude -v -p $(DEVICE) -c $(PROGRAMMER) -P $(PORT) -b $(BAUD) -U flash:w:$(FILENAME).hex:i
+	
+clean :
+	$(REMOVE) $(FILENAME).o
+	$(REMOVE) $(FILENAME).elf
+	$(REMOVE) $(FILENAME).hex
